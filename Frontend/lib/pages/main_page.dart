a5a7450988b4f94defa31b15864d39160725ad03
chyuje96 <74063824+chyuje96@users.noreply.github.com>
2022-10-25 17:19:48 +0900

    :bug: 머지 에러 수정

--- Frontend/lib/pages/main_page.dart
+++ Frontend/lib/pages/main_page.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/materiaart';
+import 'package:flutter/material.dart';
+import 'package:homealone/components/main/carousel.dart';
 import 'package:homealone/components/main/main_button_down.dart';
 import 'package:homealone/components/main/main_button_up.dart';
 import 'package:homealone/components/main/profile_bar.dart';
